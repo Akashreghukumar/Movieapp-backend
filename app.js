d89b952469aa0ba3118ee136dff61c58b07840cd
Akashreghukumar <47936922+Akashreghukumar@users.noreply.github.com>
2022-01-09 18:08:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+var createError = require('http-errors');
+
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
 const express=require("express");
-const app=express()
-
-
 
 const mongo=require('./shared/connection');
 
@@ -13,6 +16,28 @@ const userRouter=require('./routes/user')
 const authorize=require('./module/authorize')
 
 const movieRouter=require('./routes/movie')
+
+
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const port=process.env.PORT || 3001
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 
 app.use(express.json())
 // console.log(mongo)
@@ -26,4 +51,24 @@ app.use(express.json())
 //  app.use(authorize.isadmin)
 app.use('/user',userRouter)
 app.use('/movie',movieRouter)
-app.listen(3001) 
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+
+
+  
+});
+
+module.exports = app;
