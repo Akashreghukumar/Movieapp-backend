cf597b348b4d3c82b83ab80d6f5a3dafb209f11d
Akashreghukumar <47936922+Akashreghukumar@users.noreply.github.com>
2022-01-09 18:03:47 +0530

    Update connection.js

--- shared/connection.js
+++ shared/connection.js
@@ -1,7 +1,9 @@
 const mongoose=require('mongoose')
 exports.connect=()=>{
     try{
-        mongoose.connect('mongodb://localhost:27017/movieappdb')
+       // mongoose.connect('mongodb://localhost:27017/movieappdb')
+       mongoose.connect('mongodb+srv://akash:iBLUozExk7Duh5jE@cluster0.756jt.mongodb.net/movie?retryWrites=true&w=majority')
+
         console.log("monggosse mondb conneced")
 
     }
